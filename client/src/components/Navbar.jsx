98684997790c35fa21fbf2634e6ef907e986e65b
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-01-25 18:25:20 +0700

    feature : sidebar and navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,14 +10,22 @@ import {
 import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "../state";
-import profileImage from "assets/profile.jpg";
+import profileImage from "../assets/profile.jpg";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+} from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
   return (
-    <Appbar
+    <AppBar
       sx={{
         position: "static",
         background: "none",
@@ -26,8 +34,42 @@ const Navbar = () => {
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+          <IconButton>
+            <Avatar src={profileImage} sx={{ width: "32px", height: "32px" }} />
+            Bocchi
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
-    </Appbar>
+    </AppBar>
   );
 };
 
--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  return <p>Dashboard</p>;
+};
 
-export default Dashboard
+export default Dashboard;

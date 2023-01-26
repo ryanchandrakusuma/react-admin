6735ff074fce8aad7d39c80bc6fbfa4aa6ace63b
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-01-26 21:18:48 +0700

    feature : get user
    injecting mock data and get certain user

--- server/index.js
+++ server/index.js
@@ -1,37 +1,46 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
-import generalRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/client.js";
 import salesRoutes from "./routes/client.js";
+
+//data
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
 
 // configuration
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // routes
-app.use("/client", clientRoutes);
+// app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes);
+// app.use("/management", managementRoutes);
+// app.use("/sales", salesRoutes);
 
 // mongoose setup
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server PORT : ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`));
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server PORT : ${PORT}`));
+
+    User.insertMany(dataUser);
+  })
+  .catch((error) => console.log(`${error} did not connect`));

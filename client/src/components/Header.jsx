02d5251e52e6a4558ed5de803baa04b64a84cbf0
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-02-17 14:18:29 +0700

    update : product page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Typography, Box, useTheme } from "@mui/material";
 import React from "react";
 
 const Header = ({ title, subtitle }) => {
-  const theme = useTheme;
+  const theme = useTheme();
   return (
     <Box>
       <Typography
         variant="h2"
-        color={theme.palette.secondary[200]}
+        color={theme.palette.secondary[100]}
         fontWeight="bold"
         sx={{ mb: "5px" }}
       >
